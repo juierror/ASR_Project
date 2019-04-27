10c66ec695a0f5f88334fa8d6c9ec987bffda3b1
juierror <siwa.boonpun@gmail.com>
2019-04-27 06:03:25 +0000

    WER 85%

--- jui.sh
+++ jui.sh
@@ -3,3 +3,6 @@ rm -rf data/lang_nnet
 ngram -lm LM/lm.lm -write-lm LM/lm.arpa
 gzip LM/lm.arpa
 ngram -lm LM/lm.lm -write-lm LM/lm.arpa
+./prepare_LG.sh lexicon_words.txt LM/lm.arpa exp/nnet2_online/phones.txt data/local/dict_nnet data/lang_nnet
+rm -rf data/lang_nnet_g
+./utils/format_lm.sh data/lang_nnet LM/lm.arpa.gz lexicon_words.txt data/lang_nnet_g
