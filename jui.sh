5c476488eeff4227b8e280c148361b499d9c4431
juierror <siwa.boonpun@gmail.com>
2019-04-29 08:54:00 +0000

    WER 45%

--- jui.sh
+++ jui.sh
@@ -1,8 +1,10 @@
 rm -rf data/local/dict_nnet
 rm -rf data/lang_nnet
+ngram-count -text LM/transcript_mac_2.txt -order 3 -lm LM/lm.lm
 ngram -lm LM/lm.lm -write-lm LM/lm.arpa
 gzip LM/lm.arpa
 ngram -lm LM/lm.lm -write-lm LM/lm.arpa
 ./prepare_LG.sh lexicon_words.txt LM/lm.arpa exp/nnet2_online/phones.txt data/local/dict_nnet data/lang_nnet
 rm -rf data/lang_nnet_g
 ./utils/format_lm.sh data/lang_nnet LM/lm.arpa.gz lexicon_words.txt data/lang_nnet_g
+utils/mkgraph.sh data/lang_nnet_g exp/nnet2_online exp/nnet2_online/graph
---
+++ jui_class.sh
@@ -0,0 +1,10 @@
+rm -rf data/local/dict_nnet
+rm -rf data/lang_nnet
+./LM/build_class_ngram ./LM/asr.manual.classes ./LM/transcript_mac.txt 3 ./LM/asr.vocab ./LM/lm.lm
+ngram -lm tmp.lm -write-lm lm.arpa
+gzip lm.arpa
+ngram -lm tmp.lm -write-lm lm.arpa
+./prepare_LG.sh lexicon_words.txt lm.arpa exp/nnet2_online/phones.txt data/local/dict_nnet data/lang_nnet
+rm -rf data/lang_nnet_g
+./utils/format_lm.sh data/lang_nnet lm.arpa.gz lexicon_words.txt data/lang_nnet_g
+utils/mkgraph.sh data/lang_nnet_g exp/nnet2_online exp/nnet2_online/graph
